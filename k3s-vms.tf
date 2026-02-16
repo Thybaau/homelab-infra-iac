f0bc038d8c0c8e70630d8be4a0a9ce5f0d046fc1
Thybaau <thibaultcaremelle@gmail.com>
2026-02-16 08:50:08 +0100

    Add k3s and openclaw vms terraform files

---
+++ k3s-vms.tf
@@ -0,0 +1,57 @@
+resource "proxmox_vm_qemu" "k3s_nodes" {
+  count = var.k3s_count
+
+  # Identification
+  name        = "k3s-node-${format("%02d", count.index + 1)}"
+  target_node = var.proxmox_node
+  vmid        = 200 + count.index
+
+  # Template
+  clone = var.template_name
+
+  # Ressources CPU/RAM
+  memory  = var.k3s_vm_memory
+  cores   = var.k3s_vm_cores
+  sockets = 1
+  cpu     = "host"
+
+  # Démarrage automatique
+  onboot  = true
+  startup = "order=1"
+
+  # Disque
+  disk {
+    type    = "scsi"
+    storage = var.storage_pool
+    size    = var.k3s_vm_disk_size
+    format  = "qcow2"
+    cache   = "writeback"
+  }
+
+  # Cloud-Init
+  os_type                 = "cloud-init"
+  cloudinit_cdrom_storage = var.storage_pool
+
+  # Réseau
+  network {
+    model  = "virtio"
+    bridge = var.network_bridge
+  }
+
+  # Cloud-Init - Configuration réseau
+  ipconfig0 = "ip=${cidrhost("192.168.1.0/24", 102 + count.index)}/24,gw=${var.network_gateway}"
+
+  nameserver = var.network_dns
+
+  # Cloud-Init - Configuration utilisateur
+  ciuser  = var.vm_user
+  sshkeys = var.ssh_public_key
+
+  # Lifecycle
+  lifecycle {
+    ignore_changes = [
+      network,
+      disk,
+    ]
+  }
+}
---
+++ openclaw-vm.tf
@@ -0,0 +1,55 @@
+resource "proxmox_vm_qemu" "openclaw" {
+  # Identification
+  name        = "openclaw-01"
+  target_node = var.proxmox_node
+  vmid        = 210
+
+  # Template
+  clone = var.template_name
+
+  # Ressources
+  memory  = var.openclaw_vm_memory
+  cores   = var.openclaw_vm_cores
+  sockets = 1
+  cpu     = "host"
+
+  # Démarrage automatique
+  onboot  = true
+  startup = "order=2"
+
+  # Disque
+  disk {
+    type    = "scsi"
+    storage = var.storage_pool
+    size    = var.openclaw_vm_disk_size
+    format  = "qcow2"
+    cache   = "writeback"
+  }
+
+  # Cloud-Init
+  os_type                 = "cloud-init"
+  cloudinit_cdrom_storage = var.storage_pool
+
+  # Réseau
+  network {
+    model  = "virtio"
+    bridge = var.network_bridge
+  }
+
+  # Cloud-Init - Configuration réseau
+  ipconfig0 = "ip=${var.openclaw_vm_ip}/24,gw=${var.network_gateway}"
+
+  nameserver = var.network_dns
+
+  # Cloud-Init - Configuration utilisateur
+  ciuser  = var.vm_user
+  sshkeys = var.ssh_public_key
+
+  # Lifecycle
+  lifecycle {
+    ignore_changes = [
+      network,
+      disk,
+    ]
+  }
+}
